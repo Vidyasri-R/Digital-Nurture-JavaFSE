95bb6db7f17c07d67a9422efe11b543c6b738c8a
Vidyasri-R <vidyasriravikumar12@gmail.com>
2026-07-21 22:12:30 +0530

    week 6

---
+++ Week 6/ticketbookingapp/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginControl from './Components/LoginControl';
+
+function App() {
+    return (
+        <div>
+            <LoginControl />
+        </div>
+    );
+}
+export default App;
